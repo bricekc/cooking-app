d2723867bd03fd67b862f43c4d09d1c2349de9e8
CorentinCLERO <corentinclero@hotmail.com>
2025-11-07 16:23:09 +0100

    display all recipes

--- src/app/recipe/recipes/recipes.component.ts
+++ src/app/recipe/recipes/recipes.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RecipeService } from '../../shared/services/recipe.service';
+import { Recipe } from '../../shared/models/recipe';
+import { RecipeCardComponent } from "../../shared/ui/recipe-card/recipe-card.component";
 
 @Component({
   selector: 'app-recipes',
-  imports: [],
+  imports: [RecipeCardComponent],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrl: './recipes.component.css',
 })
-export class RecipesComponent {
 
+export class RecipesComponent implements OnInit {
+  private recipeSvc = inject(RecipeService);
+  recipes = signal<Recipe[]>([]);
+
+  ngOnInit(): void {
+    this.recipeSvc.getAllRecipies().subscribe((recipes) => {
+      this.recipes.set(recipes);
+    });
+  }
 }
--- src/app/shared/services/recipe.service.ts
+++ src/app/shared/services/recipe.service.ts
@@ -1,9 +1,50 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { combineLatest, map } from 'rxjs';
+import { Recipe, GetIngredient, GetRecipe, Category } from '../models/recipe';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
+  private readonly API_URL = 'http://localhost:3000';
+  private http = inject(HttpClient);
+  recipes = signal<Recipe[]>([]);
 
-  constructor() { }
+  getAllRecipies() {
+    return combineLatest([
+      this.http.get<GetRecipe[]>(`${this.API_URL}/recipes`),
+      this.http.get<GetIngredient[]>(`${this.API_URL}/ingredients`),
+      this.http.get<Category[]>(`${this.API_URL}/categories`),
+    ]).pipe(
+      map(
+        ([recipes, ingredients, categories]: [
+          GetRecipe[],
+          GetIngredient[],
+          Category[]
+        ]) => {
+          const recipesContent: Recipe[] = [];
+
+          for (const recipe of recipes) {
+            const formatedRecipe: Recipe = {
+              ...recipe,
+              category: categories.filter(
+                (category) => category.id === recipe.categoryId
+              )[0].category,
+              ingredients: recipe.ingredients.map((recipeIngredient) => ({
+                ...recipeIngredient,
+                name: ingredients.filter(
+                  (ingredient) => ingredient.id === recipeIngredient.id
+                )[0].name,
+              })),
+            };
+            recipesContent.push(formatedRecipe);
+          }
+
+          console.log(recipesContent)
+          return recipesContent;
+        }
+      )
+    );
+  }
 }
