84b49d55d8ab2890cb1f8f5122aba482d9269d9a
CorentinCLERO <corentinclero@hotmail.com>
2025-11-07 12:06:48 +0100

    init database and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipesComponent } from './recipe/recipes/recipes.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '/login',
+    component: LoginComponent
+  },
+  {
+    path: '/register',
+    component: RegisterComponent
+  },
+  {
+    path: '/recipes',
+    component: RecipesComponent
+  },
+  {
+    path: '/recipe/add',
+    component: RecipeFormComponent
+  },
+  {
+    path: '/recipe/:id',
+    component: RecipeDetailsComponent
+  }
+];
